0cacebeff0b21a0ea871555067bd70c6929e0c08
Simon Buchholz <simon.buchholz@xaasbase.com>
2017-03-06 16:50:59 +0100

    Refactoring and minor changes before final submission of the project

--- README.md
+++ README.md
@@ -1,12 +1,17 @@
 # Ethereum contract event listener
 [![Build Status](http://ec2-54-194-144-141.eu-west-1.compute.amazonaws.com/api/badges/cp2017/monitoring-event-listener/status.svg)](http://ec2-54-194-144-141.eu-west-1.compute.amazonaws.com/cp2017/monitoring-event-listener)
-This NPM module can subscribe to events that are fired by contracts in the Ethereum blockchain. At this
-time the module can only be used as a CLI tool.
+This little node script is part of the Cloud Prototyping Project at TU Berlin (ISE). It represents a monitor that
+randomly gets jobs from the Monitoring-Contract of the service marketplace. As soon as the monitor receives a job it
+starts monitoring the availability of a provided endpoint URL. After a certain time period has passed, the script
+writes back the result as percentage of of availability over the time period.
+
+The project is based on this generic ethereum event listener: 
+https://www.npmjs.com/package/ethereum-listener
 
 ## Installation
 At the moment there are two possibilities to run the CLI tool.
 
-https://www.npmjs.com/package/ethereum-listener
+https://www.npmjs.com/package/cp2017-service-monitor
 
 ### 1. Run the script locally
 First download the project from Github and navigate to the project directory. 
@@ -17,18 +22,18 @@ Then simply run:
 ### 2. Install as global NPM package
 Install the CLI tool as a global NPM packe by running:
 
-`$ npm install ethereum-listener -g`
+`$ npm install cp2017-service-monitor -g`
 
 Now you can easily start the script by typing:
 
-`$ ethereum-listener`
+`$ cp2017-service-monitor`
 
 ## Getting started
 ### 1. Start ethereum client
-To talk to an ethereum node from inside the Ethereum Listener we use the web3.js library, which gives an convenient
+To talk to an ethereum node from inside this monitoring script we use the web3.js library, which gives an convenient
 interface for the RPC methods. In order to make it work, you need to follow the following instructions.
 
-Start your client with the --rpc flag. Because we are accessing the RPC interface from the Ethereum Listener, 
+Start your ethereum client with the --rpc flag. Because we are accessing the RPC interface from the monitoring-script, 
 CORS will need to be enabled like:
 `geth --rpc --rpcapi db,eth,net,web3,personal,shh --shh --rpccorsdomain "http://localhost:4200"`
 
@@ -39,26 +44,36 @@ If you don't want to use the default port (8545), you can change the default HTT
 #### 2.a Provide parameters
 Here is an example how you can provide the information as arguments when starting the script:
 
-`$ ethereum-listener  --ethereumProvider="http://localhost:8545" --password="pw0" --contractAddress="7596bcd41d32d170c572349c47d6c3f78004dbd6" --eventName="newMonitorRecord" --contractAbi=contract-abi`
+`$ cp2017-service-monitor --ethereumProvider="http://localhost:8545" --password="pw0" --contractAddress="7596bcd41d32d170c572349c47d6c3f78004dbd6" --contractAbi=contract-abi`
 
 Be aware that the `--contractAbi` parameter needs to get the address to a file in your file system (which contains the abi) instead of the ABI itself. 
 Also make sure that you remove the prefix 0x of the contract address. 
 
 #### 2.a Use interactive CLI
-The usage of this script is self explaining. Start the script by `$ ethereum-listener`. It will ask you for the address of your ethereum client and the
-password for the first user (the one at eth.accounts[0]). Then it asks for the contract address as well as
-for the contract abi. The last thing you need to provide is the name of the event that you want to subscribe to.
+If you don't provide the parameters when starting the monitor, it will start an interactive CLI where you can pass all
+the parameters one after another. Start the script by `$ cp2017-service-monitor`. It will ask you for the address
+of your ethereum client and the password for the first user (the one at eth.accounts[0]). Then it asks for the contract
+address as well as for the contract abi.
+
+Here is an example how to use the script with the CLI interface:
 
 ```
-  _____   _     _                                                  _   _         _                                
- | ____| | |_  | |__     ___   _ __    ___   _   _   _ __ ___     | | (_)  ___  | |_    ___   _ __     ___   _ __ 
- |  _|   | __| | '_ \   / _ \ | '__|  / _ \ | | | | | '_ ` _ \    | | | | / __| | __|  / _ \ | '_ \   / _ \ | '__|
- | |___  | |_  | | | | |  __/ | |    |  __/ | |_| | | | | | | |   | | | | \__ \ | |_  |  __/ | | | | |  __/ | |   
- |_____|  \__| |_| |_|  \___| |_|     \___|  \__,_| |_| |_| |_|   |_| |_| |___/  \__|  \___| |_| |_|  \___| |_|   
-                                                                                                                                                                                                                       
+  ____                          _                   __  __                   _   _
+ / ___|    ___   _ __  __   __ (_)   ___    ___    |  \/  |   ___    _ __   (_) | |_    ___    _ __
+ \___ \   / _ \ | '__| \ \ / / | |  / __|  / _ \   | |\/| |  / _ \  | '_ \  | | | __|  / _ \  | '__|
+  ___) | |  __/ | |     \ V /  | | | (__  |  __/   | |  | | | (_) | | | | | | | | |_  | (_) | | |
+ |____/   \___| |_|      \_/   |_|  \___|  \___|   |_|  |_|  \___/  |_| |_| |_|  \__|  \___/  |_|
+
+You did not provide the arguments (ethereumProvider, password, contractAddress, contractAbi).
 ? Enter your ethereum client address: http://localhost:8545
 ? Enter your ethereum user password: ***
-? Enter the address of the contract that you want to listen to: 0x593d12e2676e50b0254346f1bac6ff1257323706
+? Enter the address of the contract that you want to listen to: 0xd6e3763357a75e106f84a8125c91f53aeced28e2
 ? Enter the ABI of the contract that you want to listen to: Received
-? Enter the name of the event that you want to subscribe to: newMonitorRecord
+Subscribing to newMonitorRecord
+Subscribing to jobMonitorEvent
+New monitoring jobs available
+Asking the contract for a job: Get monitor request called
+Transaction hash: 0x11b6f4270584fa0120b891d5dafdc6de37111ede21b9b1b534940c40c2a9a186
+New job recieved. Index: 9
+Start monitoring: http://localhost:3000/api/monitor
 ```
--- monitor.js
+++ monitor.js
@@ -2,7 +2,6 @@
  Libraries
  */
 const chalk = require('chalk');
-const figlet = require('figlet');
 const inquirer = require('inquirer');
 const Web3 = require('web3');
 const http = require('http');
@@ -15,38 +14,29 @@ let accountPassword;
 let web3;
 let contractAddress;
 let abi;
-let eventFunctionName;
 let contract;
 let event;
 let parametersProvided = false;
 
-
 module.exports = {
-
     startCli: () => {
-        console.log("You did not provide the arguments (ethereumProvider, password, contractAddress, contractAbi, eventName).");
+        console.log("You did not provide the arguments (ethereumProvider, password, contractAddress, contractAbi).");
         initEthereum();
     },
-    processArguments: (provider, password, contractAdd, contractAbi, eventName) => {
+    processArguments: (provider, password, contractAdd, contractAbi) => {
         console.log('Everything provided as arguments');
         accountPassword = password;
         ethereumProvider = provider;
         contractAddress = "0x" + contractAdd;
         abi = JSON.parse(contractAbi);
-        eventFunctionName = eventName;
         parametersProvided = true;
         initEthereum();
-    },
-    printHeadline: () => {
-        console.log(
-            chalk.blue(
-                figlet.textSync('Service Monitor', {horizontalLayout: 'full'})
-            )
-        );
     }
-
 };
 
+/********************************************************************
+ 1. Ask for the etherum client address and initialize the web3 library
+ *******************************************************************/
 function initEthereum() {
     if (!parametersProvided) {
         getEthereumProviderFromCli(getWeb3);
@@ -55,6 +45,9 @@ function initEthereum() {
     }
 }
 
+/**
+ * Connect to the ethereum client
+ */
 function getWeb3() {
     if (!parametersProvided) {
         accountPassword = arguments[0].password;
@@ -80,6 +73,9 @@ function getWeb3() {
     });
 }
 
+/**
+ * Unlocks the default account of the ethereum client
+ */
 function unlockDefaultAccount() {
     web3.personal.unlockAccount(web3.eth.defaultAccount, accountPassword, 999, ((error, result) => {
         if (!error) {
@@ -113,7 +109,6 @@ function getContract() {
         try {
             contractAddress = arguments[0].contractAddress;
             abi = JSON.parse(arguments[0].contractAbi);
-            eventFunctionName = arguments[0].eventName;
             web3.personal.unlockAccount(web3.eth.defaultAccount, accountPassword, 9999, eventListener);
         } catch (err) {
             console.error(chalk.red("Couldn't process the arguments. Please check contract address, abi and event name!"));
@@ -143,9 +138,13 @@ function eventListener(error, result) {
     }
 }
 
+
+/**
+ * Registering an event listener to the newMonitorRecord event of the monitoring-contract
+ */
 function subscribeNewMonitorRecordEvent() {
     try {
-        event = contract[eventFunctionName]();
+        event = contract.newMonitorRecord();
         console.log("Subscribing to newMonitorRecord");
         //    console.log(event);
         event.watch((error, eventResult) => {
@@ -162,6 +161,7 @@ function subscribeNewMonitorRecordEvent() {
                         console.error(chalk.red(getMonitorRequestError));
                     } else {
                         console.log("Asking the contract for a job: Get monitor request called");
+                        console.log("Transaction hash: " + getMonitorRequestRes)
                     }
                 })
             }
@@ -177,6 +177,10 @@ function subscribeNewMonitorRecordEvent() {
         }
     }
 }
+
+/**
+ * Registering an event listener to the jobMonitorEvent event of the monitoring-contract
+ */
 function subscribeJobMonitorEvent() {
     try {
         jobMonitorEvent = contract.jobMonitorEvent();
@@ -196,8 +200,7 @@ function subscribeJobMonitorEvent() {
                     if (!monitorJobUrl) {
                         console.error(chalk.red("Monitor job error: it does not contain an endpoint URL."));
                     } else {
-
-                        monitorEndpoint(monitorJobUrl, jobIndex);
+                        monitorEndpoint(monitorJobUrl + "/monitor", jobIndex);
                     }
                 }
             }
@@ -217,8 +220,17 @@ function subscribeJobMonitorEvent() {
 /************************************************************************
  * The actual monitoring function
  *************************************************************************/
+/**
+ * This function monitors the endpoint for a certain time period and calls a function of the monitoring contract
+ * to write back the result in percent of availability over the monitored time period.
+ * @param monitorJobUrl The endpoint that needs to be monitored
+ * @param jobIndex The monitoring job id that was provided when the contract gave us the job
+ */
+// TODO To check whether the user is authorized to use this endpoint, it would be nice if
+// TODO the contract passes a (by the user) pre-signed message to the monitor.
 function monitorEndpoint(monitorJobUrl, jobIndex) {
     console.log("Start monitoring: " + monitorJobUrl);
+    // TODO the monitoring period should be set by the contract and not by the monitor itself
     // Monitoring for 30 minutes
     //  let testPeriod = 1800000;
     let testPeriod = 60000;
@@ -268,9 +280,12 @@ function monitorEndpoint(monitorJobUrl, jobIndex) {
 }
 
 /**********************************************************************
- * CLI functions
+ * Interactive CLI functions: In case the use did not provide parameters
+ * on script start this functions can be used to input the parameters
+ * interactively one after another.
  **********************************************************************/
 
+// Ask for the ethereum client address and password of the default user
 function getEthereumProviderFromCli(callback) {
     let questions = [
         {
@@ -302,7 +317,10 @@ function getEthereumProviderFromCli(callback) {
     inquirer.prompt(questions).then(callback);
 }
 
-
+/**
+ * Asks for the contract address of the MonitoringContract as well as for its ABI
+ * @param callback Function that gets called when the questions are answered.
+ */
 function getContractArgumentsFromCli(callback) {
     let questions = [
         {
@@ -321,18 +339,6 @@ function getContractArgumentsFromCli(callback) {
             name: 'contractAbi',
             type: 'editor',
             message: 'Enter the ABI of the contract that you want to listen to:',
-            validate: (value) => {
-                if (value.length) {
-                    return true;
-                } else {
-                    return 'Please enter the ABI of the contract that you want to listen to:';
-                }
-            }
-        },
-        {
-            name: 'eventName',
-            type: 'input',
-            message: 'Enter the name of the event that you want to subscribe to:',
             validate: (value) => {
                 if (value.length) {
                     return true;
